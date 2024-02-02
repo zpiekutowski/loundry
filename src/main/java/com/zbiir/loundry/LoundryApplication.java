2842bd9779bcb1c21a79949159f0a024312ed30d
Zbigniew Piekutowski <zbiirp@gmail.com>
2024-02-02 19:17:11 +0100

    First Commit

---
+++ src/main/java/com/zbiir/loundry/LoundryApplication.java
@@ -0,0 +1,13 @@
+package com.zbiir.loundry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoundryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoundryApplication.class, args);
+	}
+
+}
